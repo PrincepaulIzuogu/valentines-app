833fac85f3c670edab455e67046db32115582624
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2025-02-09 04:22:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import GalleryPage from './components/GalleryPage';
 import NextGalleryPage from './components/NextGalleryPage';
