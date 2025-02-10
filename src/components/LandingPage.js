84f5288d3953d8e1d482241fc1086ec240f28952
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2025-02-10 19:52:05 +0100

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import '../styles/LandingPage.css';
 import backgroundVideo from '../assets/videos/video_1.mp4';
 import backgroundAudio from '../assets/audios/audio_1.mp3';
+import backgroundImage from '../assets/images/img_0.jpg';
 
 const LandingPage = () => {
   const [showIntro, setShowIntro] = useState(true);
@@ -23,9 +24,10 @@ const LandingPage = () => {
   };
 
   return (
-    <div className="landing-container">
+    <div className="landing-container" style={{ backgroundImage: showIntro ? `url(${backgroundImage})` : 'none', backgroundSize: 'cover', backgroundPosition: 'center' }}>
       {/* Background Video */}
-      <video 
+      {!showIntro && (
+        <video 
         autoPlay 
         loop 
         muted 
@@ -38,6 +40,7 @@ const LandingPage = () => {
         <source src={backgroundVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      )}
 
       {/* Background Audio */}
       <audio ref={audioRef} loop>
@@ -56,7 +59,7 @@ const LandingPage = () => {
           <h1>Does prince know Odysseus so well?</h1>
           <p>Come on, let's find out!!</p>
           <Link to="/gallery">
-            <button className="bounce-button">Let's Go Odysseus!!</button>
+            <button className="bounce-button">Click me, Odysseus!</button>
           </Link>
         </div>
       )}
