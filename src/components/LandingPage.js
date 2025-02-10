46c7ed5927ea0b1fc2f116ef8dca3bb27fdae989
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2025-02-10 20:18:22 +0100

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,41 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/LandingPage.css';
 import backgroundVideo from '../assets/videos/video_1.mp4';
-import backgroundImage from '../assets/images/img_0.jpg';
-import backgroundAudio from '../assets/audios/audio_1.mp3';
 
 const LandingPage = () => {
-  const [showIntro, setShowIntro] = useState(true);
-  const audioRef = React.useRef(null);
-
   useEffect(() => {
     console.log("Video path:", backgroundVideo);
-    if (audioRef.current) {
-      audioRef.current.volume = 0.5;
-    }
   }, []);
-
-  const startExperience = () => {
-    setShowIntro(false);
-    if (audioRef.current) {
-      audioRef.current.play().catch(error => console.log("Autoplay blocked. Waiting for user interaction."));
-    }
-  };
 
   return (
     <div className="landing-container">
-      {showIntro ? (
-        <div className="intro-section" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <h1>Welcome Odysseus :)</h1>
-          <p>I developed this site from scratch just for you</p>
-          <h1>Let's make it more romantic for this day :)</h1>
-          <button className="bounce-button" onClick={startExperience}>Click me, Ody!!!</button>
-        </div>
-      ) : (
-        <div className="main-content">
-          {/* Background Video */}
-          <video 
+      {/* Background Video */}
+      <video 
         autoPlay 
         loop 
         muted 
@@ -48,19 +24,12 @@ const LandingPage = () => {
         <source src={backgroundVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-          <h1>Does prince know Odysseus so well?</h1>
-          <p>Come on, let's find out!!</p>
-          <Link to="/gallery">
-            <button className="bounce-button">Click me, Odysseus!</button>
-          </Link>
-        </div>
-      )}
 
-      {/* Background Audio */}
-      <audio ref={audioRef} loop>
-        <source src={backgroundAudio} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+      <h1>Does prince know Odysseus so well?</h1>
+      <p>Come on, let's find out!!</p>
+      <Link to="/gallery">
+        <button className="bounce-button">Click me, Odysseus!</button>
+      </Link>
     </div>
   );
 };
